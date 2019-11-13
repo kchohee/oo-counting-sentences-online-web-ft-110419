8b9a446c322bec2f3373a5180641a7617fc76867
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-13 21:59:35 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,7 +3,7 @@ require 'pry'
 class String
 
   def sentence?
-    self.end_with?(".") ? (return "true") : (return "false")
+    self.end_with?(".") ? (return true) : (return false)
   end
 
   def question?
