066f2c225380c76b93f0f4e9e093e3dc21c628bd
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-13 21:57:41 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,7 +3,7 @@ require 'pry'
 class String
 
   def sentence?
-    if self.end_with?(".") ? (return "true") : (return "false")
+    self.end_with?(".") ? (return "true") : (return "false")
   end
 
   def question?
